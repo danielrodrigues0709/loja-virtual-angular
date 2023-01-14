5143a6eac8559765a5e29859b0af179f42105e69
Daniel <danielrodrigues0709@gmail.com>
2023-01-14 14:14:58 -0300

    build: primeiro commit com estrutura de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShowcaseComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowcaseComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
